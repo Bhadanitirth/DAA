1bc05a9273e4c556a929acc59e26bd4d7936324d
TIRTH ALPESHBHAI BHADANI <143957573+Bhadanitirth@users.noreply.github.com>
2024-03-12 16:36:57 +0530

    Update EX9_n-queens_.cpp

--- EX9_n-queens_.cpp
+++ EX9_n-queens_.cpp
@@ -1,66 +1,48 @@
 #include <bits/stdc++.h>
-#define N 4
+#define MAX 10
 using namespace std;
-void printSolution(int board[N][N])
-{
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-		if(board[i][j])
-			cout << "Q ";
-		else cout<<". ";
-		printf("\n");
-	}
-}
-bool isSafe(int board[N][N], int row, int col)
-{
-	int i, j;
 
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
-
-	return true;
-}
-bool solveNQUtil(int board[N][N], int col)
-{
-	if (col >= N)
-		return true;
-	for (int i = 0; i < N; i++) {
-		if (isSafe(board, i, col)) {
-			board[i][col] = 1;
-			if (solveNQUtil(board, col + 1))
-				return true;
-			board[i][col] = 0; 
+int arr[MAX], no;
+void nQueens(int k, int n);
+bool canPlace(int k, int i);
+void display(int n);
+void nQueens(int k, int n){
+	for (int i = 1;i <= n;i++){
+		if (canPlace(k, i)){
+			arr[k] = i;
+			if (k == n)
+				display(n);
+			else
+				nQueens(k + 1, n);
 		}
 	}
-	return false;
 }
-bool solveNQ()
-{
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
-
-	if (solveNQUtil(board, 0) == false) {
-		cout << "Solution does not exist";
+bool canPlace(int k, int i){
+	for (int j = 1;j <= k - 1;j++){
+		if (arr[j] == i || 
+			(abs(arr[j] - i) == abs(j - k)))
 		return false;
 	}
-
-	printSolution(board);
 	return true;
 }
-int main()
-{
+void display(int n){
+	cout << "Arrangement No. " << ++no<<endl;
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= n; j++){
+			if (arr[i] != j)
+				cout << "\t_";
+			else
+				cout << "\tQ";
+		}
+		cout << endl;
+	}
+}
+int main(){
     clock_t t;
     t=clock();
-	    solveNQ();
+	int n;
+	cin>>n;
+	nQueens(1, n); 
 	double time =((double)t)/CLOCKS_PER_SEC;
     cout<<time<<endl;
 	return 0;
